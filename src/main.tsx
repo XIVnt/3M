fa7e08efdd354321a3a9d4bd4ac890de199bfeb1
michael andre vega pozo <michael.vega@altia.es>
2026-05-21 12:10:13 +0200

    fix leaflet css via CDN

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
 import { AuthProvider } from "./context/AuthContext";
-import { ToastProvider } from "./context/ToastContext"; // 🔥 NUEVO
+import { ToastProvider } from "./context/ToastContext";
 
 import App from "./App";
 
-import "./api/leafletFix";
-import "leaflet/dist/leaflet.css?url";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
